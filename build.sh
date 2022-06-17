2ff78fae71696fc31a0e394b23587b490b52c855
Mark Hobson <markhobson@gmail.com>
2022-06-17 13:12:23 +0100

    WIP: Add tests

--- build.sh
+++ build.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-docker build --pull -t maven-chrome:jdk-8 jdk-8 \
-	&& docker build --pull -t maven-chrome:jdk-11 jdk-11 \
-	&& docker build --pull -t maven-chrome:jdk-17 jdk-17 \
-	&& docker build --pull -t maven-chrome:jdk-18 -t maven-chrome jdk-18
+set -e
+
+# jdk-8
+
+docker build --pull -t maven-chrome:jdk-8 jdk-8
+TAG=jdk-8 bats test
+
+# jdk-11
+
+docker build --pull -t maven-chrome:jdk-11 jdk-11
+TAG=jdk-11 bats test
+
+# jdk-17
+
+docker build --pull -t maven-chrome:jdk-17 jdk-17
+TAG=jdk-17 bats test
+
+# jdk-18
+
+docker build --pull -t maven-chrome:jdk-18 -t maven-chrome jdk-18
+TAG=jdk-18 bats test
